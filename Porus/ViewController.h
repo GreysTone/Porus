c469fe1ad5d05e81e4e411327d05f52435ab5990
Danyang Song (Arthur) <arthur.r.song@gmail.com>
2016-11-25 22:25:47 -0800

    Initial Commit

---
+++ Porus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Porus
+//
+//  Created by GreysTone on 2016-11-25.
+//  Copyright © 2016 Danyang Song (Arthur). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Porus/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Porus
+//
+//  Created by GreysTone on 2016-11-25.
+//  Copyright © 2016 Danyang Song (Arthur). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
